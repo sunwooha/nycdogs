eb8b8c5832cad69a18c7c597188a2ee436d81c22
sunwooha <sunwoo.ha15@ncf.edu>
2018-04-14 23:54:29 -0400

    added second visualization

---
+++ bar.js
@@ -0,0 +1,84 @@
+var margin = {top: 45, right: 100, bottom: 20, left: 20},
+    width = 450 - margin.left - margin.right,
+    height = 90 - margin.top - margin.bottom;
+
+var color = d3.scale.threshold()
+    .range(['#fffafa','#f5e7e8','#ead5d6','#e0c4c5','#d5b1b4','#caa0a3','#bf8f92','#b47e82','#a96d72','#9d5c63'])
+    .domain([0, 400, 800, 1200, 1600, 2000, 2400, 2800, 3200, 4000]);
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+// Scales. Note the inverted domain fo y-scale: bigger is up!
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>" + "</strong><span style='color:#fff'>" + d.n + " dogs are " + d.AgeAsOf2015 + "! </span>";
+  })
+
+d3.csv("data/age_distribution.csv", type, function(data) {
+  // Data is nested by country
+  var boroughs = d3.nest()
+      .key(function(d) { return d.Borough; })
+      .entries(data);
+  // Compute the minimum and maximum age and n across symbols.
+  x.domain(data.map(function(d) { return d.AgeAsOf2015; }));
+  y.domain([0, d3.max(boroughs, function(s) { return s.values[0].n; })]);
+
+  // Add an SVG element for each borough, with the desired dimensions and margin.
+  var svg = d3.select("#barvis").selectAll("svg")
+    .data(boroughs)
+    .enter()
+    .append("svg:svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+    .append("text")
+    .attr("x", width + 10)
+    .attr("y", height/3)
+    .attr("dy", ".71em")
+    .attr("text-anchor", "start")
+    .attr("font-size", "1.1em")
+    .text(function(d) { return d.key});
+
+  svg.selectAll(".bar")
+      .data(function(d) {return d.values;})
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.AgeAsOf2015); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.n); })
+      .attr("height", function(d) { return height - y(d.n); })
+      .attr("fill", function(d) {return color(d.n)})
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide)
+
+  svg.call(tip);
+
+});
+
+function type(d) {
+  d.n = +d.n;
+  return d;
+}
---
+++ bubble.js
@@ -0,0 +1,168 @@
+var width1 = 1000,
+        height1 = 800,
+        padding = 1.5, // separation between same-color nodes
+        clusterPadding = 6, // separation between different-color nodes
+        maxRadius = 9;
+
+    var colorScale = d3.scale.ordinal()
+        .range(["#584B53", "#9D5C63", "#D6E3F8", "#94958B", "#E4BB97"]);
+
+    d3.text("data/breeds_borough_count.csv", function(error, text) {
+        if (error) throw error;
+        var colNames = "group,text,size\n" + text;
+        var data = d3.csv.parse(colNames);
+
+    data.forEach(function(d) {
+        d.size = +d.size;
+    });
+
+    //unique cluster/group id's
+    var cs = [];
+    data.forEach(function(d){
+        if(!cs.includes(d.group)) {
+            cs.push(d.group);
+        }
+    });
+
+    var n = data.length, // total number of nodes
+        m = cs.length; // number of distinct clusters
+
+    //create clusters and nodes
+    var clusters = new Array(m);
+    var nodes = [];
+    for (var i = 0; i<n; i++){
+        nodes.push(create_nodes(data,i));
+    }
+
+    var force = d3.layout.force()
+        .nodes(nodes)
+        .size([width1, height1])
+        .gravity(.02)
+        .charge(0)
+        .on("tick", tick)
+        .start();
+
+    var svg = d3.select("#bubblevis").append("svg")
+        .attr("width", width1)
+        .attr("height", height1)
+
+    var node = svg.selectAll("circle")
+        .data(nodes)
+        .enter().append("g")
+        .call(force.drag);
+
+    node.append("circle")
+        .style("fill", function (d) {
+            return colorScale(d.cluster);
+        })
+        .attr("r", function(d){return d.radius})
+        .on("mouseover", function(d){
+            d3.select(this).style("opacity", 0.75);
+            updateDogInfo(d)})
+        .on("mouseout", function(d){
+            d3.select(this).style("opacity", 1);
+            updateDogInfo(d)});
+
+    svg.append("g")
+        .attr("class", "legendOrdinal")
+        .attr("transform", "translate(5,10)");
+
+    var ordinalLegend = d3.scale.ordinal()
+        .domain(["Queens", "Brooklyn", "The Bronx", "Manhattan","Staten Island"])
+        .range([ "#E4BB97", "#D6E3F8", "#9D5C63", "#94958B", "#584B53"]);
+
+    var legendOrdinal = d3.legend.color()
+        .labelFormat(d3.format(".0f"))
+        .scale(ordinalLegend)
+        .shapePadding(5)
+        .shapeWidth(20)
+        .shapeHeight(20)
+        .labelOffset(12);
+
+    svg.select(".legendOrdinal")
+        .call(legendOrdinal);
+
+    function updateDogInfo(cir) {
+        var info = "";
+        if (cir) {
+            info = [cir.text, cir.size].join(": ");
+        }
+        else{
+            info = "";
+        }
+        d3.select("#dog-info").html(info);
+    };
+
+    function create_nodes(data,node_counter) {
+    var i = cs.indexOf(data[node_counter].group),
+        r = Math.sqrt((i + 1) / m * -Math.log(Math.random())) * maxRadius,
+        d = {
+            borough: data[node_counter].group,
+            text: data[node_counter].text,
+            size: data[node_counter].size, 
+            cluster: i,
+            radius: Math.log2(data[node_counter].size)*(maxRadius/(5)),
+            x: Math.cos(i / m * 2 * Math.PI) * 100 + width1 / 2 + Math.random(),
+            y: Math.sin(i / m * 2 * Math.PI) * 100 + height1 / 2 + Math.random()
+        };
+    if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
+    return d;
+    };
+
+    function tick(e) {
+        node.each(cluster(10 * e.alpha * e.alpha))
+            .each(collide(.5))
+        .attr("transform", function (d) {
+            var k = "translate(" + d.x + "," + d.y + ")";
+            return k;
+        })
+
+    }
+
+    // Move d to be adjacent to the cluster node.
+    function cluster(alpha) {
+        return function (d) {
+            var cluster = clusters[d.cluster];
+            if (cluster === d) return;
+            var x = d.x - cluster.x,
+                y = d.y - cluster.y,
+                l = Math.sqrt(x * x + y * y),
+                r = d.radius + cluster.radius;
+            if (l != r) {
+                l = (l - r) / l * alpha;
+                d.x -= x *= l;
+                d.y -= y *= l;
+                cluster.x += x;
+                cluster.y += y;
+            }
+        };
+    }
+
+    // Resolves collisions between d and all other circles.
+    function collide(alpha) {
+        var quadtree = d3.geom.quadtree(nodes);
+        return function (d) {
+            var r = d.radius + maxRadius + Math.max(padding, clusterPadding),
+                nx1 = d.x - r,
+                nx2 = d.x + r,
+                ny1 = d.y - r,
+                ny2 = d.y + r;
+            quadtree.visit(function (quad, x1, y1, x2, y2) {
+                if (quad.point && (quad.point !== d)) {
+                    var x = d.x - quad.point.x,
+                        y = d.y - quad.point.y,
+                        l = Math.sqrt(x * x + y * y),
+                        r = d.radius + quad.point.radius + (d.cluster === quad.point.cluster ? padding : clusterPadding);
+                    if (l < r) {
+                        l = (l - r) / l * alpha;
+                        d.x -= x *= l;
+                        d.y -= y *= l;
+                        quad.point.x += x;
+                        quad.point.y += y;
+                    }
+                }
+                return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+            });
+        };
+    }
+    });         
